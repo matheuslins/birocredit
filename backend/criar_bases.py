30e2e9ad4f680b3cdeb2bccc131c0c33ab5cccfb
Matheus Lins <msl@cin.ufpe.br>
2018-08-04 17:01:53 -0300

    Muda o nome da pasta apps para modules e organiza script criarbase

--- backend/criar_bases.py
+++ backend/criar_bases.py
@@ -19,51 +19,63 @@ def criar_dados():
 def criar_pessoas():
     for index in range(QUANTIDADE_PESSOAS):
         cidadao = gerapessoa()
-        endereco = requests.post(
-            POINT + '/endereco/',
-            json={
-                "rua": cidadao['endereco']['logradouro'],
-                "numero": cidadao['endereco']['numero'],
-                "cidade": cidadao['endereco']['cidade'],
-                "estado": cidadao['endereco']['uf'],
-                "cep": cidadao['endereco']['cep']
-            },
-            headers={'Content-Type': 'application/json'}
-        )
+        endereco = criar_endereco(cidadao)
+        pessoa = criar_pessoa(cidadao, endereco)
 
-        def _idade(data_nascimento):
-            ano_nasceu = int(data_nascimento.split('/')[-1])
-            ano_atual = datetime.datetime.now().year
-            return ano_atual - ano_nasceu
-
-        dados = {
-            "nome": cidadao['nome'],
-            "cpf": cidadao['documentos']['cpf'],
-            "idade": _idade(cidadao['nascimento']),
-            "fonte_renda": "Trabalho",
-            "endereco": endereco.json()['id']
-        }
-        pessoa = requests.post(
-            POINT + '/pessoas/',
-            data=json.dumps(dados),
-            headers={'Content-Type': 'application/json'}
-        )
-        tipos_bens = ['apartamento', 'carro', 'acao', 'casa']
-        dados_bem_pessoa = {
-            "pessoa": pessoa.json()['id'],
-            "tipo": random.choice(tipos_bens),
-            "valor": random.randrange(1000, 200000)
-        }
-        requests.post(
-            POINT + '/bens/',
-            data=json.dumps(dados_bem_pessoa),
-            headers={'Content-Type': 'application/json'}
-        )
+        criar_bem(pessoa)
         # TODO: Criar eventos
         if pessoa.status_code == 201:
             print ('Pessoa Criada: {}'.format(index + 1))
         else:
             print('Erro ao criar pessoa')
+
+
+def criar_bem(pessoa):
+    tipos_bens = ['apartamento', 'carro', 'acao', 'casa']
+    dados_bem_pessoa = {
+        "pessoa": pessoa.json()['id'],
+        "tipo": random.choice(tipos_bens),
+        "valor": random.randrange(1000, 200000)
+    }
+    requests.post(
+        POINT + '/bens/',
+        data=json.dumps(dados_bem_pessoa),
+        headers={'Content-Type': 'application/json'}
+    )
+
+
+def criar_pessoa(cidadao, endereco):
+    def _idade(data_nascimento):
+        ano_nasceu = int(data_nascimento.split('/')[-1])
+        ano_atual = datetime.datetime.now().year
+        return ano_atual - ano_nasceu
+
+    dados = {
+        "nome": cidadao['nome'],
+        "cpf": cidadao['documentos']['cpf'],
+        "idade": _idade(cidadao['nascimento']),
+        "fonte_renda": "Trabalho",
+        "endereco": endereco.json()['id']
+    }
+    return requests.post(
+        POINT + '/pessoas/',
+        data=json.dumps(dados),
+        headers={'Content-Type': 'application/json'}
+    )
+
+
+def criar_endereco(cidadao):
+    return requests.post(
+        POINT + '/endereco/',
+        json={
+            "rua": cidadao['endereco']['logradouro'],
+            "numero": cidadao['endereco']['numero'],
+            "cidade": cidadao['endereco']['cidade'],
+            "estado": cidadao['endereco']['uf'],
+            "cep": cidadao['endereco']['cep']
+        },
+        headers={'Content-Type': 'application/json'}
+    )
 
 if __name__ == "__main__":
     criar_dados()
